93f730963ff32f4816489e95e96fcf5465fa4b07
Jean-Chrisrophe Ozanne <jeanchristopheozanne@gmail.com>
2025-01-22 11:40:51 +0100

    fix email validation in ShowSummary route

--- server.py
+++ server.py
@@ -26,7 +26,11 @@ def index():
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
+    try:
+        club = [club for club in clubs if club['email'] == request.form['email']][0]
+    except IndexError:
+        flash("email not found or invalid please try again")
+        return redirect(url_for('index'))
     return render_template('welcome.html',club=club,competitions=competitions)
 
 
@@ -57,3 +61,6 @@ def purchasePlaces():
 @app.route('/logout')
 def logout():
     return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ tests/unit_tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+from server import app
+
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    return app.test_client()
